840d50cd569ef87290a394584e88b2846e4eaae2
Anders Goncalves da Silva <andersgs@gmail.com>
2015-09-04 10:57:43 +1000

    Logic for finding megahit command to run

--- src/megahit.py
+++ src/megahit.py
@@ -4,12 +4,21 @@ A class that wraps around megahit to perform de novo genome assemblies
 
 class Megahit:
 
-    def __init__(self):
+    def __init__(self, cmd_path = None):
         '''
         Create an instance of Megahit
         '''
-        self.exists = False
-        # default values used in nullarbor
+        # gives the option of automatically searching for megahit in the
+        # path or for the user to provide a unique path to the megahit
+        # executable
+        # the class initially assumes that megahit is not in the path,
+        # and that the user will provide the path. when trying to run
+        # for the first time, if a command path is not provided, we search in
+        # PATH and throw an exception if nothing found
+        self.exists_in_path = False
+        self.cmd_path = cmd_path
+        # the class holds paramter values to set megahit options
+        # default values set here are those used in nullarbor
         self.k_min = 41
         self.k_max = 101
         self.k_step = 20
@@ -19,10 +28,20 @@ class Megahit:
         self.n_cpus = 16
         return
 
-    def inpath(self):
+    def in_path(self):
         '''
         Check if megahit exists in the path, and can be return.
         '''
+        for path in os.environ["PATH"].split(os.pathsep):
+            path = path.strip('"')
+            exe_file = os.path.join(path, "megahit")
+            if os.path.isfile(exe_file):
+                self.exists_in_path = True
+            else:
+                raise RuntimeError('''
+                Could find an executable for megahit. Please
+                make sure it is in your path.
+                ''')
         return
 
     def create_cmd(self):
